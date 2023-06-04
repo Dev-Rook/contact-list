3e924298261299eeafc88494e5ef0b243eab8004
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2023-06-03 19:09:51 -0500

    Made the input for the contact grid a separate component

--- src/sections/ContactGrid.jsx
+++ src/sections/ContactGrid.jsx
@@ -9,22 +9,20 @@ import DummyContacts from "../data/DummyContact.json";
 
 // Material UI Import:
 import SearchIcon from "@mui/icons-material/Search";
+import TagIcon from '@mui/icons-material/Tag';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+// Component Import:
+import Input from "../components/Input"
 
 const ContactGrid = () => {
   const [data, setData] = useState(DummyContacts);
   const [search, setSearch] = useState("");
-  console.log(search)
+  console.log(search);
 
   return (
     <div className="section">
-      <div className={styles.inputContainer}>
-      <SearchIcon sx={{color: "black"}} />
-        <input
-          type="text"
-          className={styles.input}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </div>
+      <Input setSearch={setSearch} />
       <div className={styles.contactGrid}>
         {data
           ?.filter((item) => {
@@ -35,14 +33,16 @@ const ContactGrid = () => {
           .map((item) => (
             <div className={styles.contactCard} key={item.id}>
               <div className={styles.imageContainer}>
-                <img src="" alt="" className={styles.img} />
+                <img src={""} alt="" className={styles.img} />
               </div>
               <div className={styles.deatailsContainer}>
-                <p className={styles.name}></p>
-                <p className={styles.description}></p>
+                <p className={styles.name}>
+                  {item.first_name} {item.last_name}
+                </p>
+                <p className={styles.bio}>{item.bio.slice(0, 40)}...</p>
               </div>
               <div className={styles.callToActionContainer}>
-                
+                <button className={styles.actionButton}>Profile</button>
               </div>
             </div>
           ))}
