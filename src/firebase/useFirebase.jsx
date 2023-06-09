902d526448db1f2f2f3240d4a31edae18833f015
tylim <limteikyee@gmail.com>
2023-06-09 11:30:27 +0800

    fix useFirebase empty argument

--- src/firebase/useFirebase.jsx
+++ src/firebase/useFirebase.jsx
@@ -1,34 +1,27 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useMemo } from "react";
 
 // Firebase 🔥
 import { db } from "../firebase/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 const useFirebase = ({table}) => {
-  const [contact, setContact] = useState([]);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
 
-  const contactRef = collection(db, `${table}`);
-
-  const getContact = useCallback(async () => {
+  useMemo(async () => {
     setLoading(true);
     try {
-      const data = await getDocs(contactRef);
-      setContact(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      console.log(data);
+      const data = await getDocs(collection(db, `${table}`));
+      setData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(data());
     } catch (err) {
       setError(err.message);
-    } finally {
-      setLoading(false);
     }
-  }, []);
+    setLoading(false);
+ }, [table]);
 
-  useEffect(() => {
-    getContact();
-  }, [getContact]);
-
-  return { loading, contact, error };
+  return { loading, data, error };
 };
 
 export default useFirebase;
